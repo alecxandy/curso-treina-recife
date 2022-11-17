cfbcd9baf371078ff0844d5f0532b3f02e18e774
Leonardo <omgleo@live.coom>
2022-11-17 20:07:03 -0300

    TR-002

---
+++ src/main/java/com/treina/treina/TreinaApplication.java
@@ -0,0 +1,13 @@
+package com.treina.treina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treina/treina/TreinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treina.treina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
