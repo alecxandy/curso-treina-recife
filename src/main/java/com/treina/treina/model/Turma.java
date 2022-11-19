119a29f5f6b99555b0b2c3baf42bc5a0e7621367
alexandre gomes <alecxandy@gmail.com>
2022-11-19 08:04:41 -0300

    TR-007 criando entidade Turma

---
+++ src/main/java/com/treina/treina/model/Turma.java
@@ -0,0 +1,29 @@
+package com.treina.treina.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tb_turma")
+public class Turma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int id_aluno;
+
+    private int id_curso;
+
+    private int id_professor;
+
+    private Float valor;
+}
