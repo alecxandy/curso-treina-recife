868e7461d00165eb2e9094d311f1567780c4e40a
Leonardograut <omgleo@live.com>
2022-11-25 20:11:37 -0300

    Repository TR-020 DiaAula

---
+++ src/main/java/com/treina/treina/repository/DiaaulaRepository.java
@@ -0,0 +1,9 @@
+package com.treina.treina.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.treina.treina.model.DiaAula;
+
+public interface DiaaulaRepository extends JpaRepository <DiaAula,Long> {
+    
+}
