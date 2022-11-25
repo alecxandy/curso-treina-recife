4b72f7cf8908e95817ecc0b7e327a4a58def9af2
alexandre gomes <alecxandy@gmail.com>
2022-11-24 23:30:30 -0300

    TR-007 Corrigindo relacionamento entre as entidade Turma/Aluno

--- src/main/java/com/treina/treina/model/Turma.java
+++ src/main/java/com/treina/treina/model/Turma.java
@@ -32,6 +32,7 @@ public class Turma {
     private boolean sabado;
     private boolean domingo;
 
+
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "tb_turma_has_aluno",
             joinColumns = {@JoinColumn(name = "turma_id_fk")},
