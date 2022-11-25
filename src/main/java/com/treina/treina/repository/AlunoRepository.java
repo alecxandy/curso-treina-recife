d1099970745ec71cc266f47b8662bfa5e0e22453
alexandre gomes <alecxandy@gmail.com>
2022-11-25 20:33:59 -0300

    Criando repositorio de ALuno

---
+++ src/main/java/com/treina/treina/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.treina.treina.repository;
+
+import com.treina.treina.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
