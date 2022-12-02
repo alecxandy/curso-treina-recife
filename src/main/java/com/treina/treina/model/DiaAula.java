c2c484c039c5fb41c218dc996bef49a3665b5f65
alexandre gomes <alecxandy@gmail.com>
2022-12-01 22:57:34 -0300

    Criando clesse controller de DiaAula

--- src/main/java/com/treina/treina/model/DiaAula.java
+++ src/main/java/com/treina/treina/model/DiaAula.java
@@ -1,6 +1,7 @@
 package com.treina.treina.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 @Table(name = "tb_diaAula")
 public class DiaAula {
 
