ec9842f57b758075a7387346ce62da102e13395d
alecxandy <alecxandy@gmail.com>
2022-12-17 16:09:26 -0300

    Classe de configuração do security

---
+++ src/main/java/com/treina/treina/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.treina.treina.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override//Metodo de configuração de autenticação
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+
+    @Override//Metodo de configuração de autorização
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+
+}
---
+++ src/main/java/com/treina/treina/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.treina.treina.repository;
+
+import com.treina.treina.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
