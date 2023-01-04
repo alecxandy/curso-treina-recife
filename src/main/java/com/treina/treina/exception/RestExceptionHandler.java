5601392b5698a87cd88ddfde98ccc76a340f8be1
alecxandy <alecxandy@gmail.com>
2023-01-03 22:39:36 -0300

    Inciando validação

--- src/main/java/com/treina/treina/exception/RestExceptionHandler.java
+++ src/main/java/com/treina/treina/exception/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.treina.treina.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,5 +25,6 @@ public class RestExceptionHandler {
                         .build(), HttpStatus.BAD_REQUEST
         );
     }
+
 
 }
